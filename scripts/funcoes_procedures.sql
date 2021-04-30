7e1f85a0e3eb86c9c7a0fd0ba91a549d5582bdd8
Yane <yanelisset4@gmail.com>
2021-04-29 23:46:04 -0300

    feat: add procedure do item 3.f

---
+++ scripts/funcoes_procedures.sql
@@ -0,0 +1,35 @@
+/* Item 3.f  Funções ou procedures armazenadas */
+
+
+/* Exibir o percentual de reservas feitas por profissão do usuário*/
+CREATE OR REPLACE PROCEDURE percentReservasPorProfissao()
+language plpgsql
+as $$
+declare 
+	contagem integer;
+	valores numeric;
+	somatorio integer;	
+	profissao varchar;	
+	percentual numeric;
+	
+	cursorli cursor for select distinct(u.profissao), count(fk_idusuario) as contagem from usuario u
+	join reserva r on u.id_usuario=r.fk_idusuario group by u.profissao order by contagem desc;
+	
+BEGIN	
+for vlista in cursorli loop
+	
+	select distinct(u.profissao)into profissao from usuario u
+		join reserva r on u.id_usuario=r.fk_idusuario group by u.profissao;
+		
+	select sum(soma.contador) into somatorio from 
+			(select count(r.fk_idusuario) as contador from usuario u
+				join reserva r on u.id_usuario=r.fk_idusuario 
+				group by r.fk_idusuario order by contador desc) as soma;	
+	valores:=vlista.contagem;
+	profissao=vlista.profissao;
+	percentual:=round((valores/somatorio)*100, 2);
+	raise notice '% das reservas feitas por %', percentual || '%', profissao ;
+end loop;
+END $$;
+
+call percentReservasPorProfissao();
